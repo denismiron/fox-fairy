b1db4777cac818ae6a703b4147d04852e86e4f6c
denismiron <53906630+denismiron@users.noreply.github.com>
2022-02-13 23:22:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function openSlideMenu() {
+    document.getElementById('side-menu').style.width = '200px';
+}
+
+function closeSlideMenu() {
+    document.getElementById('side-menu').style.width = '0';
+}
